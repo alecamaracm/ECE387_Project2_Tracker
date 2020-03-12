54704dc9421444751cce9ec7ee4c33ab929ef450
Alejandro Cabrerizo <alecamar@hotmail.es>
2020-03-12 10:44:54 -0400

    Initial files

---
+++ ESP32_Code/ESP32_Code.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ESP32_Code.ino
+ Created:	3/12/2020 10:44:20
+ Author:	aleca
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
